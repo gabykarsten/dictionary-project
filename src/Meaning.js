edd1abfc418ace734b51181c47f544ae05662251
gabykarsten <karstengaby@gmail.com>
2024-05-12 23:00:54 -0700

    rendered synonyms to the dictionary app

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,14 +1,19 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 import "./Meanings.css";
 
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>
-      <p>{props.meaning.definition}</p>
+      <h3> {props.meaning.partOfSpeech}</h3>
       <p>
+        <strong>Definition:</strong> {props.meaning.definition}
+        <br />
+        <strong>Example:</strong>
         <em>{props.meaning.example}</em>
+        <br />
+        <Synonyms synonyms={props.meaning.synonyms} />
       </p>
     </div>
   );
